8b26d2d2fca866e1c6ce65423d101d6fdae6823d
DAVID LUCAS DOS SANTOS <25132827@CFP127ALUNO.LOCAL>
2025-09-09 16:59:29 -0300

    Requisitando API's

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+async function buscarImagem (raca){
+    console.log ('teste 124')
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+    const response = await fetch(url)
+    const imagens = await response.json()
+    console.log(imagens.message)
+    return imagens.message
+}
+
+
+async function mostrarImagens (){
+    const container = document.getElementById("container")
+    const raca = document.getElementById('input').value.toLowerCase()
+
+    const urls = await buscarImagem(raca)
+
+    container.replaceChildren() //Limpar antes de inserir
+
+    urls.forEach(url => {  
+        const img = document.createElement("img")
+        img.classList.add('dog')
+
+        img.src = url
+
+        container.appendChild(img)
+        
+    });
+}
